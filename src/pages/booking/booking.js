e327e104db148db7b6eaba01948b550b97442ee1
ReJMaN <118201323+ReJMaN83@users.noreply.github.com>
2026-03-15 14:45:57 +0100

    uppdaterat test.yml

--- src/pages/booking/booking.js
+++ src/pages/booking/booking.js
@@ -66,7 +66,7 @@ const handleSubmit = (e) => {
       courseTypeInput.setCustomValidity('');
     }
   }
-  console.log(document.querySelector('input[name="courseType"]'));
+  
   form.reportValidity();
 
   if (!form.checkValidity()) {
